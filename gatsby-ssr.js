43168638e1650799d00a907bf2183a1b2abd6b16
vannnyle@gmail.com <vannnyle@gmail.com>
2021-07-30 19:11:17 +0300

    feat: add grid schedule to postBodyComponents

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,14 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require('react');
 
-// You can delete this file if you're not using it
+const postBodyComponents = [
+  // eslint-disable-next-line react/jsx-filename-extension
+  <script type="text/javascript" src="https://sessionize.com/api/v2/yp5syx3f/view/GridSmart" />,
+];
+
+const SUMMIT_PAGE_NAME = '/summit-2021/';
+
+exports.onRenderBody = ({ pathname, setPostBodyComponents }) => {
+  if (pathname === SUMMIT_PAGE_NAME) {
+    setPostBodyComponents(postBodyComponents);
+  }
+};
