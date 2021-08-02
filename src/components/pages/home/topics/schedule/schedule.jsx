2952f09bf2262fa7d675a83c95b9d64f18d6c303
vannnyle@gmail.com <vannnyle@gmail.com>
2021-08-02 11:05:32 +0300

    fix: replace topics with schedule grid

---
+++ src/components/pages/home/topics/schedule/schedule.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+
+const Schedule = ({ className }) => {
+  const [scheduleHTML, setScheduleHTML] = useState(null);
+  useEffect(() => {
+    fetch('https://sessionize.com/api/v2/f3vhdjzc/view/GridSmart')
+      .then((res) => res.text())
+      .then((htmlText) => setScheduleHTML(htmlText));
+  });
+  return <div className={className} dangerouslySetInnerHTML={{ __html: scheduleHTML }} />;
+};
+
+Schedule.propTypes = {
+  className: PropTypes.string,
+};
+
+Schedule.defaultProps = {
+  className: null,
+};
+
+export default Schedule;
--- src/components/pages/home/topics/topics.jsx
+++ src/components/pages/home/topics/topics.jsx
@@ -3,33 +3,20 @@ import React from 'react';
 
 import Container from 'components/shared/container';
 import Heading from 'components/shared/heading';
-import CheckIcon from 'icons/check.inline.svg';
 
-const Topics = ({ title, items, text }) => (
+import Schedule from './schedule';
+
+const Topics = ({ title }) => (
   <section className="bg-gray-3 py-28 md:py-20" id="topics">
     <Container>
       <Heading tag="h2">{title}</Heading>
-      <div className="mt-12">
-        <ul className="grid grid-cols-2 gap-y-6 gap-x-8 md:grid-cols-1 md:gap-y-4">
-          {items.map((item, index) => (
-            <li className="flex space-x-3 text-lg font-medium leading-snug" key={index}>
-              <CheckIcon className="flex-shrink-0" />
-              <span>{item}</span>
-            </li>
-          ))}
-        </ul>
-        <span className="inline-block mt-12 text-lg font-medium text-gray-1 md:mt-8 md:text-base">
-          {text}
-        </span>
-      </div>
+      <Schedule className="mt-10" />
     </Container>
   </section>
 );
 
 Topics.propTypes = {
   title: PropTypes.string.isRequired,
-  items: PropTypes.arrayOf(PropTypes.string).isRequired,
-  text: PropTypes.string.isRequired,
 };
 
 export default Topics;
