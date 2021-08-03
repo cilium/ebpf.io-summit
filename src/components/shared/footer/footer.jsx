5f3f62dddcfa30db6a1bc11ee2964cdb23a688c9
vannnyle@gmail.com <vannnyle@gmail.com>
2021-08-03 18:49:23 +0300

    fix: update logo link in Header & Footer

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => (
       Footer
     </h2>
     <Container className="flex items-center justify-between">
-      <Link to="/">
+      <Link to="https://ebpf.io/">
         <Logo className="lg:w-auto lg:h-8" />
       </Link>
 
--- src/components/shared/header/header.jsx
+++ src/components/shared/header/header.jsx
@@ -29,7 +29,7 @@ const Header = () => (
             <nav className="relative flex items-center justify-between" aria-label="Global">
               <div className="flex items-center flex-none lg:flex-1 -mt-2.5">
                 <div className="flex items-center justify-between w-auto lg:w-full">
-                  <Link to="/">
+                  <Link to="https://ebpf.io/">
                     <span className="sr-only">eBPF</span>
                     <Logo className="w-auto h-15 lg:w-auto lg:h-8" aria-label="Logo" />
                   </Link>
