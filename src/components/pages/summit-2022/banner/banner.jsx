a5f0b06b14dcbfdfdec4f0c5da216de1b083b6ae
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2022-06-30 19:44:35 +0300

    fix: remove overflow property

--- src/components/pages/summit-2022/banner/banner.jsx
+++ src/components/pages/summit-2022/banner/banner.jsx
@@ -8,7 +8,7 @@ import honeycomb from './images/honeycomb.svg';
 import shape from './images/shape.svg';
 
 const Banner = () => (
-  <section className="my-32 lg:my-28 md:my-24 sm:my-20 overflow-hidden">
+  <section className="my-32 lg:py-28 md:py-24 sm:py-20">
     <Container className="mx-auto relative flex-col bg-orange-gradient flex items-center justify-center py-14 rounded-lg">
       <img className="absolute top-full left-3/8" src={shape} alt="" aria-hidden />
       <img className="absolute right-0 md:hidden" src={honeycomb} alt="" aria-hidden />
