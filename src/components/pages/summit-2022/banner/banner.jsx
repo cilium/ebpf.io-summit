2c1a8615fdcb30f06a473a27757cf159d3a33385
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2022-07-06 13:35:47 +0300

    fix: fix honeycomb bg

--- src/components/pages/summit-2022/banner/banner.jsx
+++ src/components/pages/summit-2022/banner/banner.jsx
@@ -29,7 +29,7 @@ const Banner = () => {
             aria-hidden
           />
           <img
-            className="absolute left-0 xl:-left-14 lg:-left-20 lg:-bottom-48 sm:-bottom-52"
+            className="absolute left-0 xl:-left-14 lg:-left-20 lg:-bottom-48 sm:-left-24 sm:-bottom-52"
             src={honeycombLeft}
             alt=""
             aria-hidden
