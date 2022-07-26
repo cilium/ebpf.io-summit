4d7f4dc3fab8692d24ab679729e8fc60918c99a6
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2022-07-26 12:44:21 +0300

    feat: add loader

--- src/components/pages/summit-2022/speaker-wall/speaker-wall.jsx
+++ src/components/pages/summit-2022/speaker-wall/speaker-wall.jsx
@@ -5,11 +5,12 @@ import Container from 'components/shared/container';
 import DangerousHtml from 'components/shared/dangerous-html';
 import Heading from 'components/shared/heading';
 
-const fetchSpeakersData = async (cb) => {
+const fetchSpeakersData = async (cb, setIsLoading) => {
   try {
     const res = await fetch(process.env.GATSBY_SESSIONIZE_URL);
     const data = await res.text();
     cb(data);
+    setIsLoading(false);
   } catch (e) {
     // eslint-disable-next-line
     console.warn('Can not fetch the speakers:', { e });
@@ -18,6 +19,7 @@ const fetchSpeakersData = async (cb) => {
 
 const SpeakerWall = ({ title }) => {
   const [speakersHTML, setSpeakersHTML] = useState('');
+  const [isSpeakerWallLoading, setIsSpeakerWallLoading] = useState(true);
 
   const modifySessionizeScript = useCallback((html) => {
     // this requires for the sessionize logic to run
@@ -30,12 +32,18 @@ const SpeakerWall = ({ title }) => {
 
   useEffect(() => {
     if (process.env.GATSBY_SESSIONIZE_URL) {
-      fetchSpeakersData(modifySessionizeScript);
+      fetchSpeakersData(modifySessionizeScript, setIsSpeakerWallLoading);
     }
-  }, [modifySessionizeScript]);
+  }, [modifySessionizeScript, setIsSpeakerWallLoading]);
 
   return (
     <section className="bg-white py-28 md:py-20" id="speakers">
+      {isSpeakerWallLoading && (
+        <div className="relative flex items-center justify-center">
+          <div className="bg-white h-[408px] lg:h-[660px] md:h-[688px] sm:h-[1213px]" />
+          <div className="absolute mt-14 h-10 w-10 animate-spin rounded-full border border-b-2 border-transparent border-b-primary-1" />
+        </div>
+      )}
       <Container className="min-h-[408px]">
         <Heading className="text-center" tag="h2">
           {title}
