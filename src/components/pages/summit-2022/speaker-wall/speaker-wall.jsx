a5b000fbde9a745311cfdbe79fddf50f2c7c9d4f
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2022-07-26 12:51:43 +0300

    Revert "feat: add loader"
    This reverts commit 4d7f4dc3fab8692d24ab679729e8fc60918c99a6.

--- src/components/pages/summit-2022/speaker-wall/speaker-wall.jsx
+++ src/components/pages/summit-2022/speaker-wall/speaker-wall.jsx
@@ -5,12 +5,11 @@ import Container from 'components/shared/container';
 import DangerousHtml from 'components/shared/dangerous-html';
 import Heading from 'components/shared/heading';
 
-const fetchSpeakersData = async (cb, setIsLoading) => {
+const fetchSpeakersData = async (cb) => {
   try {
     const res = await fetch(process.env.GATSBY_SESSIONIZE_URL);
     const data = await res.text();
     cb(data);
-    setIsLoading(false);
   } catch (e) {
     // eslint-disable-next-line
     console.warn('Can not fetch the speakers:', { e });
@@ -19,7 +18,6 @@ const fetchSpeakersData = async (cb, setIsLoading) => {
 
 const SpeakerWall = ({ title }) => {
   const [speakersHTML, setSpeakersHTML] = useState('');
-  const [isSpeakerWallLoading, setIsSpeakerWallLoading] = useState(true);
 
   const modifySessionizeScript = useCallback((html) => {
     // this requires for the sessionize logic to run
@@ -32,18 +30,12 @@ const SpeakerWall = ({ title }) => {
 
   useEffect(() => {
     if (process.env.GATSBY_SESSIONIZE_URL) {
-      fetchSpeakersData(modifySessionizeScript, setIsSpeakerWallLoading);
+      fetchSpeakersData(modifySessionizeScript);
     }
-  }, [modifySessionizeScript, setIsSpeakerWallLoading]);
+  }, [modifySessionizeScript]);
 
   return (
     <section className="bg-white py-28 md:py-20" id="speakers">
-      {isSpeakerWallLoading && (
-        <div className="relative flex items-center justify-center">
-          <div className="bg-white h-[408px] lg:h-[660px] md:h-[688px] sm:h-[1213px]" />
-          <div className="absolute mt-14 h-10 w-10 animate-spin rounded-full border border-b-2 border-transparent border-b-primary-1" />
-        </div>
-      )}
       <Container className="min-h-[408px]">
         <Heading className="text-center" tag="h2">
           {title}
