65b203b94e1e659f93b65f529bdc87cfacb8ae87
Thi Van Le <vannnyle@gmail.com>
2022-08-08 13:40:03 +0200

    feat: add sessionize schedule example

--- src/components/pages/summit-2022/information/information.jsx
+++ src/components/pages/summit-2022/information/information.jsx
@@ -1,11 +1,11 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 import Container from 'components/shared/container';
+import DangerousHtml from 'components/shared/dangerous-html';
 import Heading from 'components/shared/heading';
 
 import ConnectingIcon from './images/connecting.inline.svg';
-import hexagon from './images/hexagon.svg';
-import HourglassIcon from './images/hourglass.inline.svg';
 import SharingIcon from './images/sharing.inline.svg';
 import TrendingIcon from './images/trending.inline.svg';
 
@@ -36,58 +36,44 @@ const icons = {
   trending: TrendingIcon,
 };
 
-const Information = () => (
-  <section className="mt-14 lg:mt-12 sm:mt-10" id="information">
-    <Container className="flex flex-col items-center">
-      <div
-        id="agenda"
-        className="relative flex bg-gray-3 items-center self-stretch justify-center py-14 md:py-10 rounded-lg space-x-4 md:flex-col md:space-x-0 md:space-y-4 overflow-hidden"
-      >
-        <img
-          className="absolute -right-40 -bottom-20 lg:-right-72 sm:-right-64"
-          src={hexagon}
-          loading="lazy"
-          width={362}
-          height={416}
-          alt=""
-          aria-hidden
-        />
-        <img
-          className="absolute -left-40 lg:-left-72 -top-20 sm:-left-64"
-          src={hexagon}
-          width={362}
-          height={416}
-          loading="lazy"
-          alt=""
-          aria-hidden
-        />
-        <HourglassIcon className="shrink-0 w-[30px] h-[34px] md:w-6 md:h-7 z-10" />
-        <p className="text-3xl text-center md:text-left md:text-xl font-medium leading-none z-10">
-          Full schedule coming soon
-        </p>
-      </div>
-      <div className="mt-14 md:mt-10 flex space-x-8 lg:space-x-6 md:flex-col md:space-x-0 md:space-y-6">
-        {cardItems.map(({ iconName, title, description }, index) => {
-          const Icon = icons[iconName];
-          return (
-            <article
-              className="border border-gray-2 rounded-lg flex flex-col items-start p-8 lg:p-6 flex-1"
-              key={index}
-            >
-              <Icon className="shrink-0 w-14 h-auto" />
-              <Heading
-                tag="h3"
-                className="leading-tight text-2xl lg:text-xl md:text-xl mt-6 md:mt-4"
-              >
-                {title}
-              </Heading>
-              <p className="mt-2.5 text-lg lg:text-base leading-normal">{description}</p>
-            </article>
-          );
-        })}
-      </div>
-    </Container>
-  </section>
-);
+const Information = () => {
+  const data = useStaticQuery(graphql`
+    query sessionizeScheduleQuery {
+      sessionizeSchedule
+    }
+  `);
 
+  return (
+    <section className="mt-14 lg:mt-12 sm:mt-10" id="information">
+      <Container className="flex flex-col items-center">
+        <Heading className="text-center" tag="h2">
+          Schedule
+        </Heading>
+        {data.sessionizeSchedule && (
+          <DangerousHtml className="mt-6" html={data.sessionizeSchedule} />
+        )}
+        <div className="mt-14 md:mt-10 flex space-x-8 lg:space-x-6 md:flex-col md:space-x-0 md:space-y-6">
+          {cardItems.map(({ iconName, title, description }, index) => {
+            const Icon = icons[iconName];
+            return (
+              <article
+                className="border border-gray-2 rounded-lg flex flex-col items-start p-8 lg:p-6 flex-1"
+                key={index}
+              >
+                <Icon className="shrink-0 w-14 h-auto" />
+                <Heading
+                  tag="h3"
+                  className="leading-tight text-2xl lg:text-xl md:text-xl mt-6 md:mt-4"
+                >
+                  {title}
+                </Heading>
+                <p className="mt-2.5 text-lg lg:text-base leading-normal">{description}</p>
+              </article>
+            );
+          })}
+        </div>
+      </Container>
+    </section>
+  );
+};
 export default Information;
